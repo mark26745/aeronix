8fe3618fd475720a488b4ee61f7b4c2462561c87
mark26745 <72761972+mark26745@users.noreply.github.com>
2026-03-26 03:49:52 +0300

    fix : pushed template flake

---
+++ templates/basic/flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A custom ROS 2 project powered by AeroNix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    aeronix.url = "github:mark26745/aeronix";
+    nixpkgs.follows = "aeronix/nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      aeronix,
+    }:
+    let
+      overlays = [
+        aeronix.overlays.default
+      ];
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+
+        pkgs = import nixpkgs { inherit system overlays; };
+        # Uses helper to create the env
+        droneEnv = aeronix.lib.mkDroneEnv {
+          inherit pkgs;
+          distro = "jazzy";
+        };
+      in
+      {
+        packages.default = pkgs.symlinkJoin {
+          name = "my-drone-app";
+          paths = [ droneEnv ];
+        };
+
+        # 2. Define the development shell
+        devShells.default = pkgs.mkShell {
+          name = "aeronix-dev-shell";
+
+          # Pull in the environment built by the SDK
+          packages = [
+            droneEnv
+            pkgs.colcon
+            pkgs.gdb
+          ];
+
+          shellHook = ''
+            echo "🚀 Welcome to your AeroNix-powered workspace!"
+            echo "ROS_DISTRO is set to humble via the AeroNix SDK."
+          '';
+        };
+      }
+    );
+}
