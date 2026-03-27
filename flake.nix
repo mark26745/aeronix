a195d9d687182f3e2c833d118efec90ee85428fc
mark26745 <72761972+mark26745@users.noreply.github.com>
2026-03-27 12:59:27 +0300

    housekeeping : cleaning lib, added some gazebo-sim-overlay pkgs needed for physics

--- flake.nix
+++ flake.nix
@@ -13,9 +13,9 @@
   };
 
   inputs = {
-    nixpkgs.follows = "nix-ros-overlay/nixpkgs"; # IMPORTANT!!!
-    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
     gazebo-sim-overlay.url = "github:muellerbernd/gazebo-sim-overlay";
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
     nixgl.url = "github:nix-community/nixGL";
   };
 
@@ -77,8 +77,13 @@
             name = "drone-shell-${name}";
             packages = [ env ];
             shellHook = ''
+              echo "Shell Hook for ${name} environment is loaded!"
               export ROS_DISTRO=${name}
-              export QT_QPA_PLATFORM="xcb"
+              export QT_QPA_PLATFORM=xcb
+              export QT_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/${pkgs.qt5.qtbase.qtPluginPrefix}"
+              export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-5.15/plugins"
+              export QT_STYLE_OVERRIDE=Fusion
+              export QT_PALETTE_OVERRIDE=dark
               echo "--- Drone Lab: ${name} Environment Loaded ---"
             '';
           }
@@ -102,13 +107,21 @@
         let
           system = pkgs.system;
           nixGLIntel = nixgl.packages.${system}.nixGLIntel;
+          gz-pkgs = gazebo-sim-overlay.legacyPackages.${system};
+          rosPkgs = pkgs.rosPackages.${distro};
         in
         pkgs.rosPackages.${distro}.buildEnv {
           name = "drone-env-${distro}";
-          paths = with pkgs.rosPackages.${distro}; [
-            ament-cmake-core
-            python-cmake-module
-            desktop
+          paths = [
+            rosPkgs.rviz2
+            rosPkgs.rviz-common
+            rosPkgs.rviz-default-plugins
+            rosPkgs.python-cmake-module
+            gz-pkgs.gz-harmonic
+            gz-pkgs.sdformat_14
+            rosPkgs.ament-cmake-core
+            rosPkgs.desktop
+            rosPkgs.gazebo-msgs
             pkgs.colcon
             pkgs.mavproxy
             pkgs.droneTools.mavlink.mavp2p
